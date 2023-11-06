1eea61287fefbedc8d4c8fabd849b0e5c220eb08
N1ko1a <ivanovicn98@gmail.com>
2023-11-06 17:07:55 +0100

    Dodao sam neki remap

--- lua/nikola/core/keymaps.lua
+++ lua/nikola/core/keymaps.lua
@@ -17,3 +17,18 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+--moze da uzme ceo kod kad je boldiran i da ga pomear
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- search terms ramaning in the middle
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- half page jumping
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- foo i boo
+vim.keymap.set("x", "<leader>p", [["_dP]])
